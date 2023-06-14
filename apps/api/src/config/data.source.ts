9f6aee7ceffc54f12c1c6affdb239d79559d8409
MorveN11 <morales.patty.jose@gmail.com>
2023-06-13 23:14:21 -0400

    chore: change database port for development

--- apps/api/src/config/data.source.ts
+++ apps/api/src/config/data.source.ts
@@ -5,7 +5,7 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 export const DataSourceConfig: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
+  port: Number(process.env.DB_PORT) || 1010,
   username: process.env.DB_USER || 'user',
   password: process.env.DB_PASSWORD || 'password',
   database: process.env.DB_NAME || 'hermes',
