c973796a5a42d0da8d539ec93c4d2eaea2f593be
CristianJQ <cristian.joel.quispia.quipe@gmail.com>
2023-06-20 10:14:46 -0400

    feat: change hover icon

--- apps/client/src/app/pages/map/components/marker/Marker.tsx
+++ apps/client/src/app/pages/map/components/marker/Marker.tsx
@@ -1,6 +1,6 @@
 import RoomIcon from '@mui/icons-material/Room';
 import mapboxgl, { LngLat, Marker as MapBoxMarker } from 'mapbox-gl';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import useMap from '../../context/map/MapState';
 import MarkerButton from '../../shared-ui-components/MarkerButton';
 import CoordinatesDisplay from './components/CoordinatesDisplay';
@@ -13,6 +13,15 @@ function Marker() {
   const [isStartingPoint, setIsStartingPoint] = useState(false);
   const [coordinate, setCoordinate] = useState<LngLat>();
   const [isOpen, setIsOpen] = useState(false);
+  const [hover, setHover] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (isStartingPoint) {
+      setHover('url(../../../../../../public/hover-icon.png), auto');
+    } else {
+      setHover(null);
+    }
+  }, [isStartingPoint]);
 
   const removeMarker = () => {
     if (marker.current) {
@@ -76,18 +85,22 @@ function Marker() {
 
   return (
     <>
-      {isStartingPoint && coordinate && (
-        <CoordinatesDisplay coordinates={coordinate} />
-      )}
-      <DropDownMarker
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-        onActionSelected={handleActionSelected}
-      >
-        <MarkerButton onClick={handleOpen}>
-          <RoomIcon />
-        </MarkerButton>
-      </DropDownMarker>
+      <div
+	      className='absolute inset-0 z-50 opacity-50'
+        style={{ cursor: hover }}>
+        {isStartingPoint && coordinate && (
+          <CoordinatesDisplay coordinates={coordinate} />
+        )}
+        <DropDownMarker
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          onActionSelected={handleActionSelected}
+        >
+          <MarkerButton onClick={handleOpen}>
+            <RoomIcon />
+          </MarkerButton>
+        </DropDownMarker>
+      </div>
     </>
   );
 }
