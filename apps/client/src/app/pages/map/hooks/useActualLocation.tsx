aae3a2a54a6636e1e283e1a25d5de971fa49c2a8
Isaias Rojas <s_isaiasr@tutanota.com>
2023-06-22 01:13:06 -0400

    feat: add user live location

--- apps/client/src/app/pages/map/hooks/useActualLocation.tsx
+++ apps/client/src/app/pages/map/hooks/useActualLocation.tsx
@@ -1,16 +1,40 @@
+import { useEffect, useState } from 'react';
 import useMap from '../context/map/MapState';
 
 function useActualLocation() {
   const { map } = useMap();
+  const [longitude, setLongitude] = useState(null);
+  const [latitude, setLatitude] = useState(null);
+
+  useEffect(() => {
+    let geoWatchId = null;
+    if (map.current) {
+      geoWatchId = navigator.geolocation.watchPosition(
+        position => {
+          const lat = position.coords.latitude;
+          const long = position.coords.longitude;
+          setLatitude(lat);
+          setLongitude(long);
+        },
+        error => {
+          console.log(error);
+        },
+        {
+          enableHighAccuracy: true
+        }
+      );
+    }
+    return () => {
+      if (geoWatchId) {
+        navigator.geolocation.clearWatch(geoWatchId);
+      }
+    };
+  }, [map]);
   function goToActualLocation() {
     if (map.current) {
-      navigator.geolocation.getCurrentPosition(position => {
-        const lat = position.coords.latitude;
-        const long = position.coords.longitude;
-        map.current?.jumpTo({
-          center: [long, lat],
-          zoom: 15
-        });
+      map.current?.flyTo({
+        center: [longitude, latitude],
+        zoom: 15
       });
     }
   }
