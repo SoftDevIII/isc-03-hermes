ef915579366e1822785bd7f4a7cd6db8719b6f88
Battensalpozz <sebasthiansalinaspozzo@gmail.com>
2023-06-21 23:57:57 -0400

    feat: visual structure login

---
+++ apps/client/src/app/pages/map/components/login/TextBoxEmail.tsx
@@ -0,0 +1,12 @@
+import EmailTwoToneIcon from '@mui/icons-material/EmailTwoTone';
+import TextField from '../../shared-ui-components/Textfile';
+
+function TextBoxEmail() {
+  return (
+    <div className='flex space-x-10 p-4 border border-black rounded'>
+      <TextField type='text' required placeholder='EMAIL' />
+      <EmailTwoToneIcon />
+    </div>
+  );
+}
+export default TextBoxEmail;
