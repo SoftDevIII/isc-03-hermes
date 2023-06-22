4393749100b1d57a2de0e4d5e6824b242e944d95
Isaias Rojas <s_isaiasr@tutanota.com>
2023-06-22 09:17:51 -0400

    fix: solve of useEffect call in useUserMarker

--- apps/client/src/app/pages/map/hooks/useUserMarker.tsx
+++ apps/client/src/app/pages/map/hooks/useUserMarker.tsx
@@ -1,21 +1,14 @@
 import userMarker from '@assets/user-marker.png';
-import { useEffect } from 'react';
 import useCoordinates from '../context/coordinates/CoordinatesState';
 import useMarker from './useMarker';
 
 function useUserMarker() {
-  const { userCoordinates, setUserCoordinates } = useCoordinates();
+  const { setUserCoordinates } = useCoordinates();
   const { setMarker, removeMarker, createMarkerFromCoordinates } = useMarker({
     setCoordinates: setUserCoordinates,
     type: 'user',
     icon: userMarker
   });
-
-  useEffect(() => {
-    if (userCoordinates) {
-      createMarkerFromCoordinates(userCoordinates);
-    }
-  }, [userCoordinates, createMarkerFromCoordinates]);
 
   return {
     setUserMarker: setMarker,
