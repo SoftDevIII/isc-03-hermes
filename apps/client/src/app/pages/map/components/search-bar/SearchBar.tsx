477629dc322dfe8230aab7ddb99ecbaed9ce300d
Santiago Quiroga <quiroga.default.santiago@gmail.com>
2023-06-23 00:29:20 -0400

    fix: solve lint recommendations

--- apps/client/src/app/pages/map/components/search-bar/SearchBar.tsx
+++ apps/client/src/app/pages/map/components/search-bar/SearchBar.tsx
@@ -23,10 +23,10 @@ function SearchBar() {
       </div>
       {filterData.length !== 0 && (
         <div className='flex gap-3 bg-black/50 p-6 rounded-xl items-center w-[450px] mt-3 flex-col'>
-          {filterData.map((title, index) => (
+          {filterData.map(title => (
             <button
               className='w-full bg-black/25 text-white text-lg py-2 px-3 rounded-lg'
-              key={index}
+              key={title}
               type='button'
             >
               {title}
