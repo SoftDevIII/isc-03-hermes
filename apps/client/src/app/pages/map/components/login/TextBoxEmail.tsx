96292b0480745ce4009a299712862338ed778dfe
Battensalpozz <sebasthiansalinaspozzo@gmail.com>
2023-06-22 22:08:08 -0400

    feat: styling visual structure

--- apps/client/src/app/pages/map/components/login/TextBoxEmail.tsx
+++ apps/client/src/app/pages/map/components/login/TextBoxEmail.tsx
@@ -1,11 +1,16 @@
-import EmailTwoToneIcon from '@mui/icons-material/EmailTwoTone';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import TextField from '../../shared-ui-components/Textfile';
 
 function TextBoxEmail() {
   return (
-    <div className='flex space-x-10 p-4 border border-black rounded'>
-      <TextField type='text' required placeholder='EMAIL' />
-      <EmailTwoToneIcon />
+    <div className='flex space-x-10 p-4 border border-white rounded bg-transparent'>
+      <TextField
+        type='text'
+        required
+        placeholder='EMAIL'
+        className='bg-transparent border-none text-white  outline-none'
+      />
+      <EmailOutlinedIcon />
     </div>
   );
 }
