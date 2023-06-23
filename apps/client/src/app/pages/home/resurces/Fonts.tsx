6fc47edeaa2261c97c695383a643a587543664f9
Diego Michel Roca <aa6132788@gmail.com>
2023-06-22 23:18:54 -0400

    chore: implement the first section of the home page and the resources

---
+++ apps/client/src/app/pages/home/resurces/Fonts.tsx
@@ -0,0 +1,13 @@
+function FontRoboto(dark: boolean) {
+  if (dark) {
+    return {
+      fontFamily: "'Roboto'",
+      color: '#194569'
+    };
+  }
+  return {
+    fontFamily: "'Roboto'",
+    color: '#CADEED'
+  };
+}
+export default FontRoboto;
