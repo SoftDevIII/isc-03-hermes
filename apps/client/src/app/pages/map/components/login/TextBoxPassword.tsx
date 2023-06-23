1901b4ed938ea0dbb91e74f4f5d7bd1489ee9502
Battensalpozz <sebasthiansalinaspozzo@gmail.com>
2023-06-22 22:08:08 -0400

    feat: styling visual structure

--- apps/client/src/app/pages/map/components/login/TextBoxPassword.tsx
+++ apps/client/src/app/pages/map/components/login/TextBoxPassword.tsx
@@ -1,11 +1,16 @@
-import LockTwoToneIcon from '@mui/icons-material/LockTwoTone';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import TextField from '../../shared-ui-components/Textfile';
 
 function TextBoxPassword() {
   return (
-    <div className='flex space-x-10 p-4 border border-black rounded'>
-      <TextField type='password' required placeholder='PASSWORD' />
-      <LockTwoToneIcon />
+    <div className='flex space-x-10 p-4 border border-white rounded bg-transparent'>
+      <TextField
+        type='password'
+        required
+        placeholder='PASSWORD'
+        className='bg-transparent border-none text-white text- outline-none'
+      />
+      <LockOutlinedIcon />
     </div>
   );
 }
