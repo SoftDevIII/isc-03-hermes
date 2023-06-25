d375a796417c17e21e95059907ae8af094045cd2
JosuePrado <prado69508698@gmail.com>
2023-06-25 13:10:14 -0400

    feat: integrate the user validation with Log In page

--- apps/client/src/app/pages/map/LoginPage.tsx
+++ apps/client/src/app/pages/map/LoginPage.tsx
@@ -1,20 +1,50 @@
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import axios, { AxiosResponse } from 'axios';
+import { useState } from 'react';
 import TextBoxEmail from './components/login/TextBoxEmail';
 import TextBoxPassword from './components/login/TextBoxPassword';
 import handleLogin from './hooks/useLogin';
 import Button from './shared-ui-components/Button';
 
 function LoginPage() {
+  const [isValid, setIsValid] = useState(false);
+  const [isError, setIsError] = useState(true);
+  const handleButtonClick = async (
+    email: string,
+    password: string
+  ): Promise<void> => {
+    let postRequest: AxiosResponse;
+    try {
+      postRequest = await axios.post('/api/customer', { email, password });
+      setIsValid(Boolean(postRequest.data));
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('POST request failed:', error);
+    }
+  };
+
   return (
     <div className='flex flex-col space-y-10 border items-center bg bg-gradient-to-r from-[#194569] via-[#5F84A2] to-[#194569] p-4 md:w-1/2 lg:w-1/3 xl:w-1/4'>
       <AccountCircleOutlinedIcon style={{ fontSize: 120 }} />
       <div className='flex flex-col space-y-10'>
+        {!isError && (
+          <p hidden={isError}>
+            Acceso inválido. Por favor, inténtelo otra vez.
+          </p>
+        )}
         <TextBoxEmail id='emailRef' />
         <TextBoxPassword id='passwordRef' />
         <Button
           className='bottom-0 bg-[#194569] text-white w-full p-4 text-1xl'
           onClick={() => {
-            handleLogin('emailRef', 'passwordRef');
+            const userData = handleLogin('emailRef', 'passwordRef');
+            handleButtonClick(userData.email, userData.password).catch(
+              error => {
+                // eslint-disable-next-line no-console
+                console.error('POST request failed:', error);
+              }
+            );
+            setIsError(isValid);
           }}
         >
           LOGIN
