93d3a392f542cdf9304d45fbdad71d1d9acdfce1
DeidamiaFuentes <deidamia.fuentes.8698857@usalesiana.edu.bo>
2023-06-24 23:33:03 -0400

    feat: sign up styling visual structure

--- apps/client/src/app/pages/map/SignUpPage.tsx
+++ apps/client/src/app/pages/map/SignUpPage.tsx
@@ -5,50 +5,92 @@ import LabelTextField from './shared-ui-components/LabelTextField';
 function SignUpPage() {
   const { handleActionSelected } = useMenuActions();
   return (
-    <div className=''>
-      <div className=''>
-        <h1 className=''>Personal information</h1>
+    <div className='bg-[#194569] min-h-screen flex flex-col justify-center items-center p-4 sm:p-8'>
+      <div className='text-left w-full'>
+        <h1 className='text-left text-white px-16 text-2xl'>
+          Personal information
+        </h1>
       </div>
-      <div className=''>
-        <div className=''>
-          <LabelTextField type='string' classNameTF='' classNameL='' required>
+      <div className='w-1/2'>
+        <div className='flex space-x-6 w-full'>
+          <LabelTextField
+            type='string'
+            classNameTF='rounded w-full'
+            classNameL='text-white'
+            required
+          >
             First Name
           </LabelTextField>
-          <LabelTextField type='string' classNameTF='' classNameL='' required>
+          <LabelTextField
+            type='string'
+            classNameTF='rounded w-full'
+            classNameL='text-white'
+            required
+          >
             Last Name
           </LabelTextField>
         </div>
-        <LabelTextField type='string' classNameTF='' classNameL='' required>
+        <LabelTextField
+          type='string'
+          classNameTF='rounded w-full'
+          classNameL='text-white'
+          required
+        >
           Email
         </LabelTextField>
-        <div className=''>
-          <LabelTextField type='string' classNameTF='' classNameL='' required>
+        <div className='flex space-x-6'>
+          <LabelTextField
+            type='string'
+            classNameTF='rounded w-full'
+            classNameL='text-white'
+            required
+          >
             Birthdate
           </LabelTextField>
-          <LabelTextField type='string' classNameTF='' classNameL='' required>
+          <LabelTextField
+            type='string'
+            classNameTF='rounded w-full'
+            classNameL='text-white'
+            required
+          >
             Country
           </LabelTextField>
         </div>
       </div>
-      <div className=''>
-        <h1 className=''>User data</h1>
+      <div className='text-left w-full'>
+        <h1 className='text-left text-white px-16 text-2xl'>User data</h1>
       </div>{' '}
-      <div className=''>
-        <div className=''>
-          <LabelTextField type='string' classNameTF='' classNameL='' required>
+      <div className='space-y-6 w-1/2'>
+        <div className='w-1/2'>
+          <LabelTextField
+            type='string'
+            classNameTF='rounded w-full'
+            classNameL='text-white'
+            required
+          >
             User name
           </LabelTextField>
-          <LabelTextField type='' classNameTF='' classNameL='' required>
+          <LabelTextField
+            type='Password'
+            classNameTF='rounded w-full'
+            classNameL='text-white'
+            required
+          >
             Password
           </LabelTextField>
-          <LabelTextField type='' classNameTF='' classNameL='' required>
+          <LabelTextField
+            type='Password'
+            classNameTF='rounded w-full'
+            classNameL='text-white'
+            required
+          >
             Confirm Password
           </LabelTextField>
         </div>
       </div>
-      <div className=''>
+      <div className='w-1/4 p-5'>
         <Button
-          className=''
+          className='bg-white rounded w-full items-center'
           onClick={() => {
             handleActionSelected('a');
           }}
