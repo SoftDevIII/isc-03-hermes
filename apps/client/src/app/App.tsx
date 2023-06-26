0b14cdcc24ec6c904f4e7386b502e5289aa3ced0
JosuePrado <prado69508698@gmail.com>
2023-06-26 17:53:49 -0400

    feat: improve verification of user credentials on the login page

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import MapPage from './pages/map/MapPage';
 import NotFound from './pages/not-found/NotFound';
+import LoginPage from './pages/map/LoginPage';
 
 function App() {
   return (
     <Routes>
       <Route path='/map' element={<MapPage />} />
+      <Route path='/login' element={<LoginPage />} />
       <Route path='/' element={<Navigate to='/map' />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
--- apps/client/src/app/pages/map/LoginPage.tsx
+++ apps/client/src/app/pages/map/LoginPage.tsx
@@ -1,36 +1,45 @@
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import axios, { AxiosResponse } from 'axios';
-import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import TextBoxEmail from './components/login/TextBoxEmail';
 import TextBoxPassword from './components/login/TextBoxPassword';
 import handleLogin from './hooks/useLogin';
 import Button from './shared-ui-components/Button';
 
 function LoginPage() {
-  const [isValid, setIsValid] = useState(false);
-  const [isError, setIsError] = useState(true);
-  const handleButtonClick = async (
+  const [isCredentialValid, setIsCredentialValid] = useState(false);
+  const [isCredentialError, setIsCredentialError] = useState(true);
+  const navigate = useNavigate();
+  const userCredentialHandle = async (
     email: string,
     password: string
   ): Promise<void> => {
     let postRequest: AxiosResponse;
     try {
       postRequest = await axios.post('/api/customer', { email, password });
-      setIsValid(Boolean(postRequest.data));
+      setIsCredentialValid(Boolean(postRequest.data));
     } catch (error) {
       // eslint-disable-next-line no-console
       console.error('POST request failed:', error);
     }
   };
-
+  useEffect(() => {
+    if (isCredentialValid) {
+      navigate('/map');
+    }
+  }, [isCredentialValid, navigate]);
   return (
     <div className='flex flex-col space-y-10 border items-center bg bg-gradient-to-r from-[#194569] via-[#5F84A2] to-[#194569] p-4 md:w-1/2 lg:w-1/3 xl:w-1/4'>
       <AccountCircleOutlinedIcon style={{ fontSize: 120 }} />
       <div className='flex flex-col space-y-10'>
-        {!isError && (
-          <p hidden={isError}>
-            Acceso inválido. Por favor, inténtelo otra vez.
-          </p>
+        {!isCredentialError && (
+          <h1
+            className='text-left text-white bg-red-500'
+            hidden={isCredentialError}
+          >
+            Invalid access. Please try again.
+          </h1>
         )}
         <TextBoxEmail id='emailRef' />
         <TextBoxPassword id='passwordRef' />
@@ -38,13 +47,13 @@ function LoginPage() {
           className='bottom-0 bg-[#194569] text-white w-full p-4 text-1xl'
           onClick={() => {
             const userData = handleLogin('emailRef', 'passwordRef');
-            handleButtonClick(userData.email, userData.password).catch(
+            userCredentialHandle(userData.email, userData.password).catch(
               error => {
                 // eslint-disable-next-line no-console
                 console.error('POST request failed:', error);
               }
             );
-            setIsError(isValid);
+            setIsCredentialError(isCredentialValid);
           }}
         >
           LOGIN
