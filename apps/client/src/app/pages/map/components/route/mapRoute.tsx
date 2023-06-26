0c1a52eb98e0493bc262b67812b7a4780dc22602
Hairax <hairaxelpoderoso@gmail.com>
2023-06-26 17:03:15 -0400

    feat: create route calculation for map

--- apps/client/src/app/pages/map/components/route/mapRoute.tsx
+++ apps/client/src/app/pages/map/components/route/mapRoute.tsx
@@ -2,21 +2,21 @@ const calculateRoute = async (
   start: number[],
   end: number[]
 ): Promise<{
-  distance: number;
-  duration: number;
   route: any[];
 }> => {
   const response = await fetch(
-    `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?access_token=pk.eyJ1IjoibW9ydmVuLTExIiwiYSI6ImNsaTNtNmpueTBrYWQzcnIyZTcxYWNpNTkifQ.6yfo49_0daERompDzjTOEg&steps=true`
+    `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${
+      start[1]
+    };${end[0]},${end[1]}?access_token=${
+      import.meta.env.VITE_PUBLIC_MAPBOX_TOKEN
+    }`
   );
   const data = await response.json();
 
-  const { distance, duration, legs } = data.routes[0];
+  const { legs } = data.routes[0];
 
   const route = legs[0].steps.map((step: any) => step.maneuver.instruction);
   return {
-    distance,
-    duration,
     route
   };
 };
