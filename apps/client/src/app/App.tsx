9f220b1fb50988111236cfb7cec289549ad3c3fc
JosuePrado <prado69508698@gmail.com>
2023-06-26 17:53:49 -0400

    feat: improve verification of user credentials on the login page

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -3,10 +3,16 @@ import LoginPage from '@pages/login/LoginPage';
 import MapPage from '@pages/map/MapPage';
 import NotFound from '@pages/not-found/NotFound';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import MapPage from './pages/map/MapPage';
+import NotFound from './pages/not-found/NotFound';
+import LoginPage from './pages/map/LoginPage';
 
 function App() {
   return (
     <Routes>
+      <Route path='/map' element={<MapPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/' element={<Navigate to='/map' />} />
       <Route path='*' element={<NotFound />} />
       <Route path='/' element={<Navigate to='/map' />} />
       <Route path='/map' element={<MapPage />} />
