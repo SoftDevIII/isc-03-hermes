7c3f2a54ee3153295b79eea78eafbf19a33fad58
Battensalpozz <sebasthiansalinaspozzo@gmail.com>
2023-06-27 11:28:41 -0400

    fix: solution to comment problems

--- apps/client/src/app/pages/map/hooks/useHandleLogin.tsx
+++ apps/client/src/app/pages/map/hooks/useHandleLogin.tsx
@@ -1,7 +1,7 @@
 function handleLogin(email: string, password: string) {
   const emailRef = document.getElementById(email) as HTMLInputElement;
   const passwordRef = document.getElementById(password) as HTMLInputElement;
-  return {
+  const userData = {
     email: emailRef.value,
     password: passwordRef.value
   };
