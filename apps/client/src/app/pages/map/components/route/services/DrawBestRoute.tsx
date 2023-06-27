bed2f250ed4ad99302b8e3315566cbfba20a1c85
JheremyTancara <jheremykay777@gmail.com>
2023-06-26 22:13:56 -0400

    feat: draw best route on map

---
+++ apps/client/src/app/pages/map/components/route/services/DrawBestRoute.tsx
@@ -0,0 +1,37 @@
+import mapboxgl, { LngLatBounds } from 'mapbox-gl';
+
+const drawBestRoute = (map: mapboxgl.Map, routeData: { route: any[] }) => {
+  const coordinates = routeData.route.map((instruction: any) => {
+    const { maneuver } = instruction;
+    const { location } = maneuver;
+    return location;
+
+  map.addLayer({
+    id: 'route',
+    type: 'line',
+    source: {
+      type: 'geojson',
+      data: {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'LineString',
+          coordinates: coordinates,
+        },
+      },
+    },
+    layout: {
+      'line-join': 'round',
+      'line-cap': 'round',
+    },
+    paint: {
+      'line-color': '#888',
+      'line-width': 6,
+    },
+  });
+
+  const bounds = coordinates.reduce((bounds: LngLatBounds, coord: number[]) => {
+    return bounds.extend(coord);
+  }, new LngLatBounds(coordinates[0], coordinates[0]));
+  map.fitBounds(bounds, { padding: 50 });
+};
