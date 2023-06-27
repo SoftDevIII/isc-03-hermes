f0c6ba386ad4b1b3514829ae5fed143371e34cc3
MorveN11 <morales.patty.jose@gmail.com>
2023-06-27 15:46:12 -0400

    refactor: improve use location component

--- apps/client/src/app/pages/map/components/actual-location/ActualLocation.tsx
+++ apps/client/src/app/pages/map/components/actual-location/ActualLocation.tsx
@@ -1,72 +1,91 @@
 import MyLocationIcon from '@mui/icons-material/MyLocation';
-import { Alert, CircularProgress, Snackbar } from '@mui/material';
-import { useState } from 'react';
+import { Alert, AlertColor, CircularProgress, Snackbar } from '@mui/material';
+import { Marker } from 'mapbox-gl';
+import { useEffect, useState } from 'react';
 import useMap from '../../contexts/map/MapState';
 import ActualLocationButton from './components/ActualLocationButton';
-import useActualLocation from './hooks/useActualLocation';
+import useLocation from './hooks/useLocation';
+import useUserMarker from './hooks/useUserMarker';
 
 function ActualLocation() {
   const { isLoading } = useMap();
-  const {
-    goToActualLocation,
-    toggleUserMarker,
-    isFetchingLocation,
-    isLocationTimeout,
-    hasGeoLocation,
-    hasGeoPermission,
-    snackbarMessage,
-    snackbarType
-  } = useActualLocation();
 
-  const [isActive, setIsActive] = useState(true);
+  const [isDisabled, setIsDisabled] = useState(true);
   const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [snackbarSeverity, setSnackbarSeverity] = useState('');
+  const [marker, setMarker] = useState<Marker>(new Marker());
+  const [isMarked, setIsMarked] = useState(false);
+
+  const { createUserMarker, removeUserMarker, updateCoordinates } =
+    useUserMarker({
+      marker,
+      setMarker,
+      setIsMarked
+    });
+  const { isFetching, fetchUserLocation, getPermissions, removeLocation } =
+    useLocation({
+      createUserMarker,
+      removeUserMarker,
+      updateCoordinates
+    });
+
+  const handleFetchLocation = () => {
+    if (isMarked) {
+      removeLocation();
+      return;
+    }
+
+    fetchUserLocation({
+      setSnackbarMessage,
+      setSnackbarOpen,
+      setSnackbarSeverity
+    });
+  };
+
+  useEffect(() => {
+    getPermissions({
+      setSnackbarMessage,
+      setSnackbarOpen,
+      setSnackbarSeverity,
+      setIsDisabled
+    })
+      .then()
+      .catch((error: Error) => {
+        throw new Error(error.message);
+      });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   if (isLoading) {
     return <div />;
   }
 
-  function toggleActive() {
-    setIsActive(prevIsActive => !prevIsActive);
-    if (isActive) {
-      goToActualLocation();
-    }
-    toggleUserMarker();
-  }
-
-  const disabled =
-    isFetchingLocation ||
-    isLocationTimeout ||
-    !hasGeoLocation ||
-    !hasGeoPermission;
-
   return (
     <div className='absolute right-6 bottom-40 rounded-full md:right-8 md:bottom-44'>
       <span>
         <ActualLocationButton
-          onClick={() => {
-            if (disabled) {
-              setSnackbarOpen(true);
-            } else {
-              toggleActive();
-              setSnackbarOpen(true);
-            }
-          }}
-          disabled={disabled}
+          onClick={() => handleFetchLocation()}
+          disabled={isDisabled || isFetching}
         >
-          {isFetchingLocation ? (
+          {isFetching ? (
             <CircularProgress size={24} color='inherit' />
           ) : (
-            <MyLocationIcon color={isActive ? 'inherit' : 'primary'} />
+            <MyLocationIcon color={isMarked ? 'primary' : 'inherit'} />
           )}
         </ActualLocationButton>
       </span>
       <Snackbar
         open={snackbarOpen}
-        autoHideDuration={2000}
+        autoHideDuration={1500}
         onClose={() => setSnackbarOpen(false)}
         anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
       >
-        <Alert onClose={() => setSnackbarOpen(false)} severity={snackbarType}>
+        <Alert
+          onClose={() => setSnackbarOpen(false)}
+          severity={snackbarSeverity as AlertColor}
+        >
           {snackbarMessage}
         </Alert>
       </Snackbar>
---
+++ apps/client/src/app/pages/map/components/actual-location/enums/Status.ts
@@ -0,0 +1,7 @@
+enum Status {
+  SUCCESS = 'success',
+  WARNING = 'warning',
+  ERROR = 'error'
+}
+
+export default Status;
