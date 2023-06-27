de1c9b4afe6c901fde650a78a5701cf238edccaf
AndrePzDn <andreperezduran@gmail.com>
2023-06-27 19:15:09 -0400

    feat: improve code and resposive design

---
+++ apps/client/src/app/pages/home/header/components/MenuSideBar.tsx
@@ -0,0 +1,32 @@
+import MenuBar from '@home-assets/menu-bar.png';
+import ReferenceButton from '@home-components/ReferenceButton';
+import { useState } from 'react';
+
+function MenuSideBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <button type='button' onClick={toggleSidebar}>
+        <img src={MenuBar} className='w-5' alt='MenuBar' />
+      </button>
+      <div
+        className={` ${
+          isOpen ? 'translate-x-0' : 'hidden'
+        } sm:text-base text-sm`}
+      >
+        <div className='flex flex-col p-1'>
+          <ReferenceButton content='Home' />
+          <ReferenceButton content='About' />
+          <ReferenceButton content='Functions' />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MenuSideBar;
