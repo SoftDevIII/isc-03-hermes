aee8e5f823f239e02fcc88cfb2c789b96f4353e6
Alex Paca <fujorita772@gmail.com>
2023-06-27 18:51:25 -0400

    feat: calculate distances

---
+++ apps/client/src/app/pages/map/hooks/useDistanceByBike.tsx
@@ -0,0 +1,28 @@
+import { LngLat } from 'mapbox-gl';
+
+interface DirectionsResponse {
+  routes: {
+    distance: number;
+  }[];
+}
+
+const calculateDistanceByBike = async (
+  start: LngLat,
+  end: LngLat
+): Promise<number> => {
+  const response = await fetch(
+    `https://api.mapbox.com/directions/v5/mapbox/cycling/${start.lat},${
+      start.lng
+    };${end.lat},${end.lng}?access_token=${
+      import.meta.env.VITE_PUBLIC_MAPBOX_TOKEN
+    }`
+  );
+
+  const data = (await response.json()) as DirectionsResponse;
+
+  const { distance } = data.routes[0];
+  const distanceInKilometers = Math.round((distance / 1000) * 100) / 100;
+
+  return distanceInKilometers;
+};
+export default calculateDistanceByBike;
