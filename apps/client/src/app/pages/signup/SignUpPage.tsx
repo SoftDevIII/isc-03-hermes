2ccb2c986972cb4786d039015ade7eff320ae1bb
DeidamiaFuentes <deidamia.fuentes.8698857@usalesiana.edu.bo>
2023-06-27 16:05:37 -0400

    fix: sign up responsive

--- apps/client/src/app/pages/signup/SignUpPage.tsx
+++ apps/client/src/app/pages/signup/SignUpPage.tsx
@@ -1,20 +1,22 @@
-import handleSignUp from './hooks/useSignUp';
-import Button from './shared-ui-components/Button';
-import LabelTextField from './shared-ui-components/LabelTextField';
+import handleSignUp from '../map/hooks/useSignUp';
+import Button from '../map/shared-ui-components/Button';
+import LabelTextField from '../map/shared-ui-components/LabelTextField';
 
 function SignUpPage() {
   return (
-    <div className='bg-[#194569] min-h-screen flex flex-col justify-center items-center p-4 sm:p-8'>
+    <div className='bg-[#194569] min-h-screen w-screen flex flex-col justify-center items-center '>
       <div className='text-left w-full'>
-        <h1 className='text-left text-white px-16 text-2xl'>
+        <h1 className='text-left text-white px-3 text-2xl'>
           Personal information
         </h1>
       </div>
       <div className='w-1/2'>
-        <div className='flex space-x-6 w-full'>
+        <div className='flex w-full items-center flex-wrap justify-between'>
           <LabelTextField
             type='string'
-            classNameTF='rounded w-full'
+            classNameTFI='rounded w-full'
+            classNameTFD=''
+            classNameDiv='w-full md:w-1/2'
             classNameL='text-white'
             required
             id='fName'
@@ -23,7 +25,9 @@ function SignUpPage() {
           </LabelTextField>
           <LabelTextField
             type='string'
-            classNameTF='rounded w-full'
+            classNameTFI='rounded w-full'
+            classNameTFD='w-full'
+            classNameDiv='w-full md:w-1/2 md:pl-3'
             classNameL='text-white'
             required
             id='lName'
@@ -33,18 +37,21 @@ function SignUpPage() {
         </div>
         <LabelTextField
           type='string'
-          classNameTF='rounded w-full'
+          classNameTFI='rounded w-full'
+          classNameTFD='w-full'
           classNameL='text-white'
           required
           id='email'
         >
           Email
         </LabelTextField>
-        <div className='flex space-x-6'>
+        <div className='flex w-full flex-col md:flex-row justify-between'>
           <LabelTextField
             type='date'
-            classNameTF='rounded w-full'
+            classNameTFI='rounded w-full'
+            classNameTFD='w-full'
             classNameL='text-white'
+            classNameDiv='w-full md:w-1/2'
             required
             id='birthdate'
           >
@@ -52,8 +59,10 @@ function SignUpPage() {
           </LabelTextField>
           <LabelTextField
             type='string'
-            classNameTF='rounded w-full'
+            classNameTFI='rounded w-full'
+            classNameTFD='w-full'
             classNameL='text-white'
+            classNameDiv='w-full md:w-1/2 md:pl-3'
             required
             id='country'
           >
@@ -63,12 +72,13 @@ function SignUpPage() {
       </div>
       <div className='text-left w-full'>
         <h1 className='text-left text-white px-16 text-2xl'>User data</h1>
-      </div>{' '}
-      <div className='space-y-6 w-1/2'>
-        <div className='w-1/2'>
+      </div>
+      <div className='space-y-6 w-1/2 items-center'>
+        <div className='w-full md:w-1/2 '>
           <LabelTextField
             type='string'
-            classNameTF='rounded w-full'
+            classNameTFI='rounded w-full'
+            classNameTFD='w-full'
             classNameL='text-white'
             required
             id='userName'
@@ -77,7 +87,8 @@ function SignUpPage() {
           </LabelTextField>
           <LabelTextField
             type='Password'
-            classNameTF='rounded w-full'
+            classNameTFI='rounded w-full'
+            classNameTFD='w-full'
             classNameL='text-white'
             required
             id='password'
@@ -86,7 +97,9 @@ function SignUpPage() {
           </LabelTextField>
           <LabelTextField
             type='Password'
-            classNameTF='rounded w-full'
+            classNameTFI='rounded w-full'
+            classNameTFD='w-full'
+            classNameDiv='w-full'
             classNameL='text-white'
             required
             id='confPassword'
@@ -95,7 +108,7 @@ function SignUpPage() {
           </LabelTextField>
         </div>
       </div>
-      <div className='w-1/4 p-5'>
+      <div className='w-1/2 p-5'>
         <Button
           className='bg-white rounded w-full items-center'
           onClick={() => {
