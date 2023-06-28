b440804589f256e5ceff918e3a76371e7dc4752a
SantiQuiroga <quiroga.default.santiago@gmail.com>
2023-06-28 15:53:04 -0400

    feat: implement search by coordinates

--- apps/client/src/app/pages/map/components/search-bar/services/SearchBarService.ts
+++ apps/client/src/app/pages/map/components/search-bar/services/SearchBarService.ts
@@ -1,5 +1,19 @@
 import PlacesAPI from '@api/PlacesAPI';
 import { INIT_LAT, INIT_LONG, MAX_RESULTS } from '@utils/constants';
+import { LngLat } from 'mapbox-gl';
+
+function convertCoordinatesToFeat({
+  coordinates
+}: ConvertCoordinatesToFeatProps) {
+  return {
+    id: coordinates.toString(),
+    place_name_es: `Coordinates: ${coordinates.lng.toString()}, ${coordinates.lat.toString()}`,
+    geometry: {
+      type: 'coordinates',
+      coordinates: [coordinates.lng, coordinates.lat]
+    }
+  };
+}
 
 function fetchMapBoxPlaces({
   query,
@@ -15,6 +29,7 @@ function fetchMapBoxPlaces({
   })
     .then(({ data }: { data: PlacesResponse }) => {
       const response = data.features.slice(0, MAX_RESULTS);
+
       setFilterData(response);
     })
     .catch((error: Error) => {
@@ -22,4 +37,25 @@ function fetchMapBoxPlaces({
     });
 }
 
-export default fetchMapBoxPlaces;
+function coordinatesRegex({ coordinates }: CoordinatesRegexProps) {
+  const regex =
+    /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)\s*,\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/g;
+  return regex.test(coordinates);
+}
+
+function convertRegexToCoordinates({
+  coordinates
+}: ConvertRegexToCoordinatesProps) {
+  const regex = coordinatesRegex({ coordinates });
+  if (!regex) {
+    return null;
+  }
+  const [lng, lat] = coordinates.replace(/\s+/g, '').split(',');
+  return new LngLat(parseFloat(lng), parseFloat(lat));
+}
+
+export {
+  convertCoordinatesToFeat,
+  convertRegexToCoordinates,
+  fetchMapBoxPlaces
+};
