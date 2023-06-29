e2f582b19f6167fadfdd0c37d242eeaad487b26f
Santiago Quiroga <quiroga.default.santiago@gmail.com>
2023-06-29 06:48:02 -0400

    chore: enhace sign up UI and functionality

---
+++ apps/client/src/app/pages/sign-up/components/SubmitButton.tsx
@@ -0,0 +1,12 @@
+function SubmitButton() {
+  return (
+    <div className='w-full flex items-center justify-center'>
+      <input
+        className='bg-slate-400 text-white font-bold shadow-2xl rounded-lg px-3 mt-2'
+        type='submit'
+      />
+    </div>
+  );
+}
+
+export default SubmitButton;
