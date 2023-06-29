23820aa624bc1e16ebcf5e3bf9ddfa9c40d8e05d
Battensalpozz <sebasthiansalinaspozzo@gmail.com>
2023-06-29 11:22:29 -0400

    feat: styling visual structure Disaster Form

--- apps/client/src/app/pages/disasterForm/disasterForm.tsx
+++ apps/client/src/app/pages/disasterForm/disasterForm.tsx
@@ -4,28 +4,28 @@ import handleDisasterForm from './service/handleDisasterForm';
 
 function DisasterForm() {
   return (
-    <div className=''>
-      <div className=''>
-        <h1 className=''>Disaster Form</h1>
+    <div className='bg-[#194569] min-h-screen w-screen flex flex-col justify-center items-center '>
+      <div className='text-center w-full'>
+        <h1 className='text-center text-white px-16 text-2xl'>Disaster Form</h1>
       </div>
-      <div className=''>
+      <div className='w-1/2'>
         <LabelTextField
           type='string'
-          classNameTFI=''
-          classNameTFD=''
-          classNameL=''
+          classNameTFI='rounded w-full'
+          classNameTFD='w-full'
+          classNameL='text-white'
           required
           id='disasterName'
         >
           Disaster Name
         </LabelTextField>
-        <div className=''>
+        <div className='flex w-full flex-col md:flex-row justify-between'>
           <LabelTextField
             type='time'
-            classNameTFI=''
-            classNameTFD=''
-            classNameL=''
-            classNameDiv=''
+            classNameTFI='rounded w-full'
+            classNameTFD='w-full'
+            classNameL='text-white'
+            classNameDiv='w-full md:w-1/2'
             required
             id='duration'
           >
@@ -33,10 +33,10 @@ function DisasterForm() {
           </LabelTextField>
           <LabelTextField
             type='string'
-            classNameTFI=''
-            classNameTFD=''
-            classNameL=''
-            classNameDiv=''
+            classNameTFI='rounded w-full'
+            classNameTFD='w-full'
+            classNameL='text-white'
+            classNameDiv='w-full md:w-1/2 md:pl-3'
             required
             id='location'
           >
@@ -45,9 +45,9 @@ function DisasterForm() {
         </div>
       </div>
 
-      <div className=''>
+      <div className='w-1/2 p-5'>
         <Button
-          className=''
+          className='bg-white rounded w-full items-center'
           onClick={() => {
             handleDisasterForm('disasterName', 'duration', 'location');
           }}
