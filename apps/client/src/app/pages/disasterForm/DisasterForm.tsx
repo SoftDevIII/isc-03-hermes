634e99bd913f5ae63badba795d15ebb831bff45b
Battensalpozz <sebasthiansalinaspozzo@gmail.com>
2023-06-28 20:18:02 -0400

    feat: visual structure of Disaster Form

--- apps/client/src/app/pages/disasterForm/DisasterForm.tsx
+++ apps/client/src/app/pages/disasterForm/DisasterForm.tsx
@@ -4,48 +4,50 @@ import handleDisasterForm from './service/handleDisasterForm';
 
 function DisasterForm() {
   return (
-    <div className=''>
-      <div className=''>
-        <h1 className=''>Disaster Form</h1>
+    <div className='bg-[#194569] min-h-screen w-screen flex flex-col justify-center items-center '>
+      <div className='text-center w-full'>
+        <h1 className='text-center text-white px-16 text-2xl'>Disaster Form</h1>
       </div>
-      <div className=''>
+      <div className='w-1/2'>
         <LabelTextField
           type='string'
-          classNameTFI=''
-          classNameTFD=''
-          classNameL=''
+          classNameTFI='rounded w-full'
+          classNameTFD='w-full'
+          classNameL='text-white'
           required
           id='disasterName'
         >
           Disaster Name
         </LabelTextField>
-        <LabelTextField
-          type='time'
-          classNameTFI=''
-          classNameTFD=''
-          classNameL=''
-          classNameDiv=''
-          required
-          id='duration'
-        >
-          Duration
-        </LabelTextField>
-        <LabelTextField
-          type='string'
-          classNameTFI=''
-          classNameTFD=''
-          classNameL=''
-          classNameDiv=''
-          required
-          id='location'
-        >
-          Location (In coordinates)
-        </LabelTextField>
+        <div className='flex w-full flex-col md:flex-row justify-between'>
+          <LabelTextField
+            type='time'
+            classNameTFI='rounded w-full'
+            classNameTFD='w-full'
+            classNameL='text-white'
+            classNameDiv='w-full md:w-1/2'
+            required
+            id='duration'
+          >
+            Duration
+          </LabelTextField>
+          <LabelTextField
+            type='string'
+            classNameTFI='rounded w-full'
+            classNameTFD='w-full'
+            classNameL='text-white'
+            classNameDiv='w-full md:w-1/2 md:pl-3'
+            required
+            id='location'
+          >
+            Location (In coordinates)
+          </LabelTextField>
+        </div>
       </div>
 
-      <div className=''>
+      <div className='w-1/2 p-5'>
         <Button
-          className=''
+          className='bg-white rounded w-full items-center'
           onClick={() => {
             handleDisasterForm('disasterName', 'duration', 'location');
           }}
