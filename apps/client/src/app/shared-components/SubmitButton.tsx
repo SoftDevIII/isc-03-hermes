c0bce3a3355901ddc78df409244907304d0e84e3
Santiago Quiroga <quiroga.default.santiago@gmail.com>
2023-06-29 18:13:20 -0400

    refactor: enhace comments

--- apps/client/src/app/shared-components/SubmitButton.tsx
+++ apps/client/src/app/shared-components/SubmitButton.tsx
@@ -2,7 +2,10 @@ function SubmitButton() {
   return (
     <div className='w-full flex items-center justify-center'>
       <input
-        className='bg-slate-400 text-white font-bold shadow-2xl rounded-lg px-3 mt-2 transition duration-100 active:transform active:scale-90 cursor-pointer hover:bg-slate-500/80 active:bg-slate-500'
+        className='bg-slate-400 text-white font-bold shadow-2xl
+        rounded-lg px-3 mt-2 transition duration-100 active:transform
+        active:scale-90 cursor-pointer hover:bg-slate-500/80
+        active:bg-slate-500'
         type='submit'
       />
     </div>
