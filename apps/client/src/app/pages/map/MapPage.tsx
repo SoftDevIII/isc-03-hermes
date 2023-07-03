1b77b53d8feeeff8eff24b36ac8ef298902ca771
MorveN11 <morales.patty.jose@gmail.com>
2023-07-03 02:09:36 -0400

    feat: implement redirect

--- apps/client/src/app/pages/map/MapPage.tsx
+++ apps/client/src/app/pages/map/MapPage.tsx
@@ -4,17 +4,20 @@ import CoordinatesProvider from '@map-contexts/coordinates/CoordinatesProvider';
 import MapProvider from '@map-contexts/map/MapProvider';
 import MarkersProvider from '@map-contexts/markers/MarkersProvider';
 import '@map-styles/marker.css';
+import { useParams } from 'react-router-dom';
 import LeftControl from './components/left-control/LeftControl';
 import RightControl from './components/right-control/RightControl';
 
 function MapPage() {
+  const { long, lat } = useParams();
+
   return (
     <MapProvider>
       <CoordinatesProvider>
         <MarkersProvider>
           <div className='h-full grid font-roboto'>
             <MapComponent />
-            <SearchBar />
+            <SearchBar long={long} lat={lat} />
             <LeftControl />
             <RightControl />
           </div>
