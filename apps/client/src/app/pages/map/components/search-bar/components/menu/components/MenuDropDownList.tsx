296d9acbc94827656ba1071848fbc75bc433e875
Santiago Quiroga <quiroga.default.santiago@gmail.com>
2023-07-05 01:17:54 -0400

    fix: float menu in search bar

--- apps/client/src/app/pages/map/components/search-bar/components/menu/components/MenuDropDownList.tsx
+++ apps/client/src/app/pages/map/components/search-bar/components/menu/components/MenuDropDownList.tsx
@@ -15,7 +15,7 @@ function MenuDropDownList({
   return (
     <div
       className={`absolute grid right-0 left-0 top-0 bottom-0 h-screen bg-black/50
-    lg:bg-black/0 lg:top-10 lg:w-56 lg:h-72 lg:left-auto ${className}`}
+    lg:bg-black/0  lg:w-56 lg:h-72 lg:left-auto ${className} mt-[-10px]`}
     >
       <div
         className='portrait:w-4/6 landscape:w-5/12 landscape:lg:w-full rounded-2xl shadow-2xl
